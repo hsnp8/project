78a1af6a97a0a9ccadb20bea696030b10ea84a33
Hussain <prabalitihussain@gmail.com>
2025-04-09 14:32:13 +0530

    add 2button features

---
+++ script1.js
@@ -0,0 +1,9 @@
+let btn=document.querySelector("#btn");
+let btn1=document.querySelector("#btn1");
+btn.addEventListener("click",()=>{
+    console.log("CLICKED");
+})
+
+btn1.addEventListener("mouseover",()=>{
+    console.log("HOVERED");
+})
